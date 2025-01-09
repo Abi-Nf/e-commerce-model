8820c17c653c7e8b2a8a5aa30be66f363064e68e
Abi-Nf <hei.abigail.3@gmail.com>
2025-01-09 15:28:23 +0300

    feat: home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,41 @@
+import { bestSells, categories, featured } from '@/mock-data';
+import { Header } from './Header';
+import { Section } from './Section';
+import { SearchBar } from './SearchBar';
+import { CardCategory, CardSells } from '@/components';
+
 export const Home = () => {
   return (
-    <div></div>
+    <div className="relative w-full">
+      <Header />
+
+      <div className="m-1 p-3">
+        <SearchBar className="max-sm:flex" />
+      </div>
+
+      <Section title="Categories">
+        {
+          categories.map((category, i) => (
+            <CardCategory key={i} {...category} />
+          ))
+        }
+      </Section>
+
+      <Section title="Featured">
+        {
+          featured.map((feat, i) => (
+            <CardSells key={i} {...feat} />
+          ))
+        }
+      </Section>
+
+      <Section title="Best sell">
+        {
+          bestSells.map((bestSell, i) => (
+            <CardSells key={i} {...bestSell} />
+          ))
+        }
+      </Section>
+    </div>
   );
 }
