f69908327e3fab85b7bb9ef97467f27d5dd34e1d
Abi-Nf <hei.abigail.3@gmail.com>
2025-01-09 11:59:32 +0300

    init app

---
+++ README.md
@@ -0,0 +1,3 @@
+# E-commerce
+
+This is a presentation of a E-commerce ui made in React.js for a mobile app integration soon.
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,ts,tsx,css}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
